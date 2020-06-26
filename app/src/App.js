a5155df2cdbfa1680f2ee068a8a1502f39c7f779
Anjola Adeuyi <engr.angotobz@gmail.com>
2020-06-26 17:54:38 +0100

    UI Interface

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import classes from './App.module.css';
+import ProductData from './ProductData';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>AJ Productions</p>
+    <div className={classes.App}>
+      <header className={classes.App_header}>
+        <nav className={classes.Topbar}>
+          <img src="./amazon-logo.png" alt="Amazon logo" />
+        </nav>
+        <div className={classes.info_header}>
+          <p>An amazon store app clone by anjola adeuyi "AJ Productionss"</p>
+        </div>
       </header>
+
+      <div className={classes.MainContainer}>
+        <div className={classes.ProductPreview}>
+          <img src={ProductData.colorOptions[1].imageUrl} />
+        </div>
+        <div className={classes.ProductData}>
+          <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
+          <p className={classes.ProductDescription}>
+            {ProductData.description}
+          </p>
+
+          <h3 className={classes.SectionHeading}>Select Color</h3>
+          <div>
+            <img
+              className={[
+                classes.ProductImage,
+                classes.SelectedProductImage,
+              ].join(' ')}
+              src={ProductData.colorOptions[0].imageUrl}
+              alt={ProductData.colorOptions[0].styleName}
+            />
+            <img
+              className={classes.ProductImage}
+              src={ProductData.colorOptions[1].imageUrl}
+              alt={ProductData.colorOptions[1].styleName}
+            />
+            <img
+              className={classes.ProductImage}
+              src={ProductData.colorOptions[2].imageUrl}
+              alt={ProductData.colorOptions[2].styleName}
+            />
+            <img
+              className={classes.ProductImage}
+              src={ProductData.colorOptions[3].imageUrl}
+              alt={ProductData.colorOptions[3].styleName}
+            />
+          </div>
+        </div>
+      </div>
+
+      <footer className={classes.App_footer}>
+        <nav className={classes.Bottombar}>
+          <img src="./amazon-logo.png" alt="Amazon logo" />
+        </nav>
+      </footer>
     </div>
   );
 }
--- app/src/ProductData.js
+++ app/src/ProductData.js
@@ -1,28 +1,27 @@
 const ProductData = {
-    title: 'FitBit 19 - The Smartest Watch',
-    description: 'Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor.',
-    colorOptions: [
-        {
-            styleName: 'Black Strap',
-            imageUrl: 'https://imgur.com/iOeUBV7.png'
-        },
-        {
-            styleName: 'Red Strap',
-            imageUrl: 'https://imgur.com/PTgQlim.png'
-        },
-        {
-            styleName: 'Blue Strap',
-            imageUrl: 'https://imgur.com/Mplj1YR.png'
-        },
-        {
-            styleName: 'Purple Strap',
-            imageUrl: 'https://imgur.com/xSIK4M8.png'
-        },
-    ],
-    featureList: [
-        "Time", "Heart Rate"
-    ]
-}
+  title: 'Apple Watch Series 3 (GPS, 38mm) - The Smartest Watch',
+  description:
+    'The Apple Watch Series 3 has Retina display, GPS, Swimproof, Optical heart sensor, Stores music, Emergency SOS, S3 chip with dual-core processor and the App Store on your wrist',
+  colorOptions: [
+    {
+      styleName: 'Black Strap',
+      imageUrl: './productImage/productImage0.png',
+    },
+    {
+      styleName: 'Red Strap',
+      imageUrl: './productImage/productImage1.png',
+    },
+    {
+      styleName: 'Blue Strap',
+      imageUrl: './productImage/productImage2.png',
+    },
+    {
+      styleName: 'Purple Strap',
+      imageUrl: './productImage/productImage3.png',
+    },
+  ],
+  featureList: ['Time', 'Heart Rate'],
+};
 
 Object.freeze(ProductData); //This line of code just makes your object as a constant. No values can be updated.
 
